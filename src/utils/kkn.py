1a8742beea5f87dd1abb970466e26277bb734aad
davinjason09 <davinjasonevanraharjo@mail.ugm.ac.id>
2026-01-31 20:53:07 +0700

    chore: formatting, remove unused code

--- src/utils/kkn.py
+++ src/utils/kkn.py
@@ -76,15 +76,15 @@ class KKN:
       headers = {"X-Requested-With": "XMLHttpRequest"}
       # fmt: off
       post_data = {
-                "draw": "1", "start": "0", "length": "25", "search[value]": "", "search[regex]": "false", "dt": "{}", "simasterUGM_token": cookie,
-                "columns[0][data]": "no",                        "columns[0][name]": "", "columns[0][searchable]": "false", "columns[0][orderable]": "false", "columns[0][search][value]": "", "columns[0][search][regex]": "false",
-                "columns[1][data]": "program_nama",              "columns[1][name]": "", "columns[1][searchable]": "true",  "columns[1][orderable]": "true",  "columns[1][search][value]": "", "columns[1][search][regex]": "false",
-                "columns[2][data]": "program_mhs_judul",         "columns[2][name]": "", "columns[2][searchable]": "true",  "columns[2][orderable]": "true",  "columns[2][search][value]": "", "columns[2][search][regex]": "false",
-                "columns[3][data]": "program_jenis_id",          "columns[3][name]": "", "columns[3][searchable]": "true",  "columns[3][orderable]": "true",  "columns[3][search][value]": "", "columns[3][search][regex]": "false",
-                "columns[4][data]": "program_mhs_keberlanjutan", "columns[4][name]": "", "columns[4][searchable]": "true",  "columns[4][orderable]": "true",  "columns[4][search][value]": "", "columns[4][search][regex]": "false",
-                "columns[5][data]": "status_nama",               "columns[5][name]": "", "columns[5][searchable]": "true",  "columns[5][orderable]": "true",  "columns[5][search][value]": "", "columns[5][search][regex]": "false",
-                "columns[6][data]": "action",                    "columns[6][name]": "", "columns[6][searchable]": "false", "columns[6][orderable]": "false", "columns[6][search][value]": "", "columns[6][search][regex]": "false",
-            }
+        "draw": "1", "start": "0", "length": "25", "search[value]": "", "search[regex]": "false", "dt": "{}", "simasterUGM_token": cookie,
+        "columns[0][data]": "no",                        "columns[0][name]": "", "columns[0][searchable]": "false", "columns[0][orderable]": "false", "columns[0][search][value]": "", "columns[0][search][regex]": "false",
+        "columns[1][data]": "program_nama",              "columns[1][name]": "", "columns[1][searchable]": "true",  "columns[1][orderable]": "true",  "columns[1][search][value]": "", "columns[1][search][regex]": "false",
+        "columns[2][data]": "program_mhs_judul",         "columns[2][name]": "", "columns[2][searchable]": "true",  "columns[2][orderable]": "true",  "columns[2][search][value]": "", "columns[2][search][regex]": "false",
+        "columns[3][data]": "program_jenis_id",          "columns[3][name]": "", "columns[3][searchable]": "true",  "columns[3][orderable]": "true",  "columns[3][search][value]": "", "columns[3][search][regex]": "false",
+        "columns[4][data]": "program_mhs_keberlanjutan", "columns[4][name]": "", "columns[4][searchable]": "true",  "columns[4][orderable]": "true",  "columns[4][search][value]": "", "columns[4][search][regex]": "false",
+        "columns[5][data]": "status_nama",               "columns[5][name]": "", "columns[5][searchable]": "true",  "columns[5][orderable]": "true",  "columns[5][search][value]": "", "columns[5][search][regex]": "false",
+        "columns[6][data]": "action",                    "columns[6][name]": "", "columns[6][searchable]": "false", "columns[6][orderable]": "false", "columns[6][search][value]": "", "columns[6][search][regex]": "false",
+      }
       # fmt: on
 
       resp = await self.client.post(data_url, data=post_data, headers=headers, follow_redirects=True)
@@ -155,13 +155,6 @@ class KKN:
 
     for p_id, entries in zip(program_list, results):
       self.main_program[p_id]["entries"] = entries
-
-  # async def update_logbook_entries(self, p_id: str):
-  #     if not (new_entries := await self.get_logbook_entries_by_id(p_id)):
-  #         print_log(f"No entries found for {self.main_program[p_id]['title']}", "ERROR")
-  #         return
-  #
-  #     self.main_program[p_id]["entries"] = new_entries
 
   async def update_logbook_entries(
     self,
